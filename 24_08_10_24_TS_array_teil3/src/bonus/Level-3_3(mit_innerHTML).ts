24b8040919ebca08f49e9bc23b7e4f3f617219cd
Minyeong Jeong <megormin@gmail.com>
2024-10-08 18:36:54 +0200

    array teil3 bonus added

--- 24_08_10_24_TS_array_teil3/src/bonus/Level-3_3(mit_innerHTML).ts
+++ 24_08_10_24_TS_array_teil3/src/bonus/Level-3_3(mit_innerHTML).ts
@@ -1,33 +1,94 @@
 const inputMealName = document.querySelector(
   "#inputMealName"
 ) as HTMLInputElement;
-const btn = document.querySelector("#btn") as HTMLInputElement;
+const btnAdd = document.querySelector("#btnAdd") as HTMLInputElement;
+const btnDelete = document.querySelector("#btnDelete") as HTMLInputElement;
+const placeNumber = document.querySelector("#placeNumber") as HTMLInputElement;
 const resultWeek = document.querySelector("#resultWeek") as HTMLElement;
 const resultList = document.querySelector("#resultList") as HTMLElement;
 const selectWeek = document.querySelector("#selectWeek") as HTMLSelectElement;
 
 const mealTotalArr: string[][] = [
   ["Pizza", "Pasta"],
-  ["Bibimbap", "Kimchipancake"],
+  ["Bibimbap", "Kimchi Pancake"],
   ["Udon", "Takoyaki"],
   ["Bratwurst", "Potato Salad"],
 ];
-// console.log(mealTotalArr[2]);
 
 if (
   inputMealName &&
-  btn &&
+  btnAdd &&
   resultWeek &&
   resultList &&
   selectWeek &&
-  mealTotalArr
+  mealTotalArr &&
+  btnDelete &&
+  placeNumber
 ) {
-  selectWeek.addEventListener("change", () => console.log(selectWeek.value));
-  btn.addEventListener("click", addMeal);
+  // !===============================================
+  // !===Initialize the default week and meal list===
+  // !===============================================
 
-  function addMeal() {
-    console.log(inputMealName.value);
-    mealTotalArr[Number(selectWeek.value)].push(inputMealName.value);
-    console.log(mealTotalArr[Number(selectWeek.value)]);
-  }
+  const defaultvalue: number = 0;
+  resultWeek.textContent = `Week ${defaultvalue + 1}`;
+  placeNumber.max = mealTotalArr[defaultvalue].length.toString();
+  mealTotalArr[defaultvalue].forEach((item) => {
+    if (resultList) {
+      resultList.innerHTML += `<li>${item}</li>`;
+    }
+  });
+
+  // !================================
+  // !===== Update the week text =====
+  // !================================
+
+  selectWeek.addEventListener("change", () => {
+    resultWeek.textContent = `Week ${Number(selectWeek.value) + 1}`;
+    resultList.innerHTML = "";
+    placeNumber.max = mealTotalArr[Number(selectWeek.value)].length.toString();
+
+    mealTotalArr[Number(selectWeek.value)].forEach((item) => {
+      if (resultList) {
+        resultList.innerHTML += `<li>${item}</li>`;
+      }
+    });
+  });
+
+  // !================================
+  // !===== Update the meal list =====
+  // !================================
+
+  btnAdd.addEventListener("click", () => {
+    if (inputMealName.value) {
+      mealTotalArr[Number(selectWeek.value)].splice(
+        Number(placeNumber.value),
+        0,
+        inputMealName.value
+      );
+      placeNumber.max =
+        mealTotalArr[Number(selectWeek.value)].length.toString();
+      resultList.innerHTML = "";
+
+      mealTotalArr[Number(selectWeek.value)].forEach((item) => {
+        if (resultList) {
+          resultList.innerHTML += `<li>${item}</li>`;
+        }
+      });
+    }
+  });
+
+  btnDelete.addEventListener("click", () => {
+    if (inputMealName.value) {
+      mealTotalArr[Number(selectWeek.value)].pop();
+      placeNumber.max =
+        mealTotalArr[Number(selectWeek.value)].length.toString();
+      resultList.innerHTML = "";
+
+      mealTotalArr[Number(selectWeek.value)].forEach((item) => {
+        if (resultList) {
+          resultList.innerHTML += `<li>${item}</li>`;
+        }
+      });
+    }
+  });
 }
