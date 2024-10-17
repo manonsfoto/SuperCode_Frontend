b3de8de47e3d68528c372f5a0acc5d989450940c
Minyeong Jeong <megormin@gmail.com>
2024-10-17 11:32:14 +0200

    ts miniproject superballs refactored

--- 27_14_10_24_TS_miniprojekte/src/superballs/superballs.ts
+++ 27_14_10_24_TS_miniprojekte/src/superballs/superballs.ts
@@ -38,20 +38,20 @@ const superBalls: SuperBall[] = [
   },
 ];
 
-const btn1 = document.querySelector("#btn1") as HTMLButtonElement;
-const btn2 = document.querySelector("#btn2") as HTMLButtonElement;
-const btn3 = document.querySelector("#btn3") as HTMLButtonElement;
-const btn4 = document.querySelector("#btn4") as HTMLButtonElement;
+const allButton = document.querySelectorAll("button");
 
 const h1 = document.querySelector("h1") as HTMLElement;
 const body = document.querySelector("body") as HTMLElement;
 
-const changeColor = (index: number) => {
-  h1.style.color = superBalls[index].textColor;
-  body.style.background = superBalls[index].bgColor;
+const changeColor = () => {
+  if (allButton) {
+    for (let i = 0; i < allButton.length; i++) {
+      allButton[i]?.addEventListener("click", () => {
+        h1.style.color = superBalls[i].textColor;
+        body.style.background = superBalls[i].bgColor;
+      });
+    }
+  }
 };
 
-btn1.addEventListener("click", () => changeColor(0));
-btn2.addEventListener("click", () => changeColor(1));
-btn3.addEventListener("click", () => changeColor(2));
-btn4.addEventListener("click", () => changeColor(3));
+changeColor();
