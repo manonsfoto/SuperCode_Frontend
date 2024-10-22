edef9e5c9ab82af5bb57cdbe8d27e2ae76e6fa69
Minyeong Jeong <megormin@gmail.com>
2024-10-22 20:39:10 +0200

    clean code bonus added

--- 32_22_10_24_Clean_Code/src/Clean_Code/Level-3_2(Bonus).ts
+++ 32_22_10_24_Clean_Code/src/Clean_Code/Level-3_2(Bonus).ts
@@ -1,78 +1,46 @@
-const a = "brutto";
-const b = "steuerklasse";
+// !==========================================
+// !======  AFTER   ==========================
+// !==========================================
 
-function main(subEv: Event): void {
-  subEv.preventDefault();
-  function result(d: number, e: number) {
-    // tslint:disable-next-line
-    // tslint:disable-next-line
-    let f =
-      e === 1
-        ? d * 0.7
-        : e === 2
-        ? d * 0.75
-        : e === 3
-        ? d * 0.8
-        : e === 4
-        ? d * 0.85
-        : e === 5
-        ? d * 0.9
-        : e === 6
-        ? d * 0.95
-        : null;
+const inputBrutto = document.querySelector("#brutto") as HTMLInputElement;
+const inputSteuerklasse = document.querySelector(
+  "#steuerklasse"
+) as HTMLSelectElement;
+const result = document.querySelector("#result") as HTMLElement;
+const button = document.querySelector("button");
 
-    return f?.toFixed(2);
+const calcNetto = () => {
+  const bruttoValue: number = Number(inputBrutto.value);
+  const steuerklasseValue: number = Number(inputSteuerklasse.value);
+
+  if (bruttoValue && steuerklasseValue) {
+    let netto;
+    switch (true) {
+      case steuerklasseValue === 1:
+        netto = bruttoValue * 0.7;
+
+        break;
+      case steuerklasseValue === 2:
+        netto = bruttoValue * 0.75;
+        break;
+      case steuerklasseValue === 3:
+        netto = bruttoValue * 0.8;
+        break;
+      case steuerklasseValue === 4:
+        netto = bruttoValue * 0.85;
+        break;
+      case steuerklasseValue === 5:
+        netto = bruttoValue * 0.9;
+        break;
+      case steuerklasseValue === 6:
+        netto = bruttoValue * 0.95;
+        break;
+    }
+
+    netto = netto?.toFixed(2);
+
+    result.textContent = `Nettogehalt: ${netto} €`;
   }
+};
 
-  const g = parseFloat(
-    document.getElementById(a)
-      ? (document.getElementById(a) as any).value
-      : 3000
-  );
-  const h = parseFloat(
-    document.getElementById(b) ? (document.getElementById(b) as any).value : 1
-  );
-
-  function setHtml(elementId: string): number {
-    const element = document.getElementById(elementId) as HTMLInputElement;
-    return parseFloat(element.value || "0");
-  }
-
-  let zw = 0;
-  if (h === 1) {
-    const zw1 = g * 0.7;
-    zw = zw1;
-  } else if (h === 2) {
-    const zw2 = g * 0.75;
-    zw = zw2;
-  } else if (h === 3) {
-    const zw2 = g * 0.8;
-    zw = zw2;
-  } else if (h === 4) {
-    const zw2 = g * 0.9;
-    zw = zw2;
-  } else if (h === 5) {
-    const zw2 = g * 0.95;
-    zw = zw2;
-  } else if (h === 6) {
-    const zw2 = g * 0.75;
-    zw = zw2;
-  }
-
-  const end = zw?.toFixed(2);
-
-  const res = result(g, h);
-
-  if (end === res) {
-    console.log("refactoring worked - it´s the same");
-    //todo cleanup other code later
-    (
-      document.getElementById("result") as any
-    ).innerText = `Nettogehalt: ${res} €`;
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  form?.addEventListener("submit", main);
-});
+button?.addEventListener("click", calcNetto);
