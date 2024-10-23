2f28dbb47b0edfae9b454eb42957d27f9db8b818
Minyeong Jeong <megormin@gmail.com>
2024-10-23 17:20:31 +0200

    function2(bonus) added

--- 33_Module_2_2/src/Functions2/funtions2(Bonus).ts
+++ 33_Module_2_2/src/Functions2/funtions2(Bonus).ts
@@ -1,25 +1,38 @@
 const inputHeight = document.querySelector("#inputHeight") as HTMLInputElement;
 const inputAge = document.querySelector("#inputAge") as HTMLInputElement;
-const radio = document.querySelector(
-  'input[name="bodyShape"]:checked'
-) as HTMLInputElement;
-
+const radioButtons = document.querySelectorAll(
+  'input[name="bodyShape"]'
+) as NodeListOf<HTMLInputElement>;
 const button = document.querySelector("button") as HTMLButtonElement;
-
 const output = document.querySelector("#output") as HTMLElement;
 
-radio.addEventListener("change", () => {
-  if (radio.checked) {
-    const checkedValue = radio.value;
-    console.log(checkedValue);
+const calcIdealWeight = (
+  height: number,
+  age: number,
+  bodyShape: number
+): number => {
+  return Math.round((height - 100 + age / 10) * 0.9 * bodyShape);
+};
+
+const getBodyShapeValue = (): number | null => {
+  for (const radio of radioButtons) {
+    if (radio.checked) {
+      return Number(radio.value);
+    }
   }
-});
-button.addEventListener("click", () => {
+  return null;
+};
+
+button?.addEventListener("click", () => {
   const height = Number(inputHeight.value);
   const age = Number(inputAge.value);
+  const bodyShape = getBodyShapeValue();
 
-  if (height && age && radio.checked) {
+  if (height && age && bodyShape !== null) {
+    const idealWeight = calcIdealWeight(height, age, bodyShape);
+
+    output.textContent = `Dein Idealgewicht ist ${idealWeight}kg`;
+  } else {
+    output.textContent = "Bitte alle Felder korrekt ausfüllen.";
   }
 });
-
-console.log(radio);
