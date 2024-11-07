b4cddc1923f87a1a93a8c7537468f0768a557641
Minyeong Jeong <megormin@gmail.com>
2024-11-07 16:20:36 +0100

    promise added

--- 33_Module_2_2/src/Promise/Level-2_2.ts
+++ 33_Module_2_2/src/Promise/Level-2_2.ts
@@ -1,4 +1,3 @@
-import { af } from "date-fns/locale";
 const lorem: string =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
