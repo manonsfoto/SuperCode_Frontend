a9f28b1cb1a296c4fede2b3dac0a25f89c821c13
Minyeong Jeong <megormin@gmail.com>
2024-12-06 17:33:54 +0100

    bonus added

--- 34_Software-Testing(Plus-Skill)/src/Bonus-TDD/TDD-Level_3_1/calculateWithEmojis.spec.ts
+++ 34_Software-Testing(Plus-Skill)/src/Bonus-TDD/TDD-Level_3_1/calculateWithEmojis.spec.ts
@@ -7,21 +7,69 @@ describe("calculateWithEmojis", () => {
     const result = calculateWithEmojis("7️⃣", "➖", "2️⃣");
     expect(result).toBe(5);
   });
-  it("should return 75, if ((7️⃣7️⃣,➖,2️⃣)) is given ", () => {
+  it("should return 75, if (7️⃣7️⃣,➖,2️⃣) is given ", () => {
     const result = calculateWithEmojis("7️⃣7️⃣", "➖", "2️⃣");
     expect(result).toBe(75);
+  });
+  it("should throw Error, if  (🤮,➕,3️⃣)  is given ", () => {
+    expect(() => calculateWithEmojis("🤮", "➕", "3️⃣")).toThrow(
+      "1. Parameter konnte nicht in eine Zahl umgewandelt werden."
+    );
+  });
+  it("should throw Error, if  (7️⃣,➖, ⚠️)  is given ", () => {
+    expect(() => calculateWithEmojis("7️⃣", "➖", "⚠️")).toThrow(
+      "3. Parameter konnte nicht in eine Zahl umgewandelt werden."
+    );
+  });
+  it("should throw Error, if  (7️⃣,😵‍💫,2️⃣)  is given ", () => {
+    expect(() => calculateWithEmojis("7️⃣", "😵‍💫", "2️⃣")).toThrow(
+      "2. Parameter konnte nicht in eine Zahl umgewandelt werden."
+    );
+  });
+  it("should return 539, if (7️⃣7️⃣2️⃣,➖,2️⃣3️⃣3️⃣) is given  ", () => {
+    const result = calculateWithEmojis("7️⃣7️⃣2️⃣", "➖", "2️⃣3️⃣3️⃣");
+    expect(result).toBe(539);
   });
 });
 
 // ^==============================================
+
+const convertEmojiToNumber = (emoji: string): number => {
+  const emojiToNum = Number(
+    [...emoji]
+      .filter((elt) => {
+        const eltToNumber = Number(elt);
+        return !isNaN(eltToNumber);
+      })
+      .join("")
+  );
+
+  return emojiToNum;
+};
+
+const checkInvalidParameter = (emoji: string, index: number): void => {
+  const invalidParam1 = [...emoji].filter((elt) => {
+    const eltToNumber = Number(elt);
+    return !isNaN(eltToNumber);
+  });
+  if (invalidParam1.length === 0) {
+    throw new Error(
+      `${index}. Parameter konnte nicht in eine Zahl umgewandelt werden.`
+    );
+  }
+};
 
 const calculateWithEmojis = (
   emoji1: string,
   operationEmoji: string,
   emoji2: string
 ): number => {
-  const numEmoji1 = Number(emojiNumberList[emoji1]);
-  const numEmoji2 = Number(emojiNumberList[emoji2]);
+  checkInvalidParameter(emoji1, 1);
+  checkInvalidParameter(emoji2, 3);
+
+  const numEmoji1 = convertEmojiToNumber(emoji1);
+  const numEmoji2 = convertEmojiToNumber(emoji2);
+
   let resultNum: number = 0;
   switch (operationEmoji) {
     case "➕":
@@ -36,19 +84,10 @@ const calculateWithEmojis = (
     case "➗":
       resultNum = numEmoji1 / numEmoji2;
       break;
+    default:
+      throw new Error(
+        `2. Parameter konnte nicht in eine Zahl umgewandelt werden.`
+      );
   }
   return resultNum;
-};
-
-const emojiNumberList: Record<string, string> = {
-  "0️⃣": "0",
-  "1️⃣": "1",
-  "2️⃣": "2",
-  "3️⃣": "3",
-  "4️⃣": "4",
-  "5️⃣": "5",
-  "6️⃣": "6",
-  "7️⃣": "7",
-  "8️⃣": "8",
-  "9️⃣": "9",
 };
